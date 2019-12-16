b1f20c109d1354aa384c49d16ce5624cdf68cb38
devinlee <devinlee24@foxmail.com>
2019-12-16 16:32:14 +0800

    实体类

--- src/main/java/com/devinlee/swagger2demo/model/User.java
+++ src/main/java/com/devinlee/swagger2demo/model/User.java
@@ -1,16 +1,23 @@
 package com.devinlee.swagger2demo.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(description = "用户实体")
 public class User {
 
+    @ApiModelProperty("用户ID")
     private Long id;
 
+    @ApiModelProperty("姓名")
     private String name;
 
+    @ApiModelProperty("年龄")
     private Integer age;
 
+    @ApiModelProperty("电子邮箱")
     private String email;
 
 }
